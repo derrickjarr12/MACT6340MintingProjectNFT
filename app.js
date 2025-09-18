770e21d6190f49f7d2d59b4ecb85990c8e29f6e3
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-09-17 22:27:35 -0400

    reinstall express and nodemon

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post("/mail", (req, res) => {
+  console.log("mail button clicked");
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
