0a414ce2f5fecaf8a54e99d9988fcfadc6a399bf
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-09-19 20:05:16 -0400

    updated contact.html and contact.js with validation

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,17 +1,45 @@
 (function () {
   "use strict";
 
-  document.querySelector("#contact-form-button")
+  let form = document.querySelector('#contact-form');
+
+  document
+  .querySelector("#contact-form-button")
     .addEventListener("click", event => {
       event.preventDefault();
       event.stopPropagation();
+      let formValid = true;
+      if (!form.checkValidity()) {
+        formValid = false;
+      }
+      form.classList.add('was-validated');
+      if (formValid) {
+        sendTheEmail();
+      }
+      function sendTheEmail() {
       console.log("You clicked the Submit Button.");
-      let name = document.querySelector("#name").value;
+      let firstName = document.querySelector("#first-name").value;
+      let lastName = document.querySelector("#last-name").value;
       let email = document.querySelector("#mail").value;
       let message = document.querySelector("#msg").value;
-      console.log("Name: " + name);
+      console.log("First name: " + firstName);
+      console.log("Last name: " + lastName);
       console.log("Email: " + email);
       console.log("Message: " + message);
+    }
     });
+
+
+
+ 
+  
+
+
+
+
+
+
+
+
 
 }());
