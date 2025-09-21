17cb8cf07a9951de8ecf2e2dd65d165a855d156f
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-09-20 20:22:47 -0400

    added utils folder updated app.js, added .evn file and update gitignore

--- public/js/contact.js
+++ public/js/contact.js
@@ -5,19 +5,21 @@
 
   document
   .querySelector("#contact-form-button")
-    .addEventListener("click", event => {
+    .addEventListener("click", (event) => {
       event.preventDefault();
       event.stopPropagation();
       let formValid = true;
-      if (!form.checkValidity()) {
+    if (!form.checkValidity()) {
         formValid = false;
       }
       form.classList.add('was-validated');
       if (formValid) {
         sendTheEmail();
       }
+    });
+
       function sendTheEmail() {
-      console.log("You clicked the Submit Button.");
+      console.log("You clicked the submit button.");
       let firstName = document.querySelector("#first-name").value;
       let lastName = document.querySelector("#last-name").value;
       let email = document.querySelector("#mail").value;
@@ -27,19 +29,4 @@
       console.log("Email: " + email);
       console.log("Message: " + message);
     }
-    });
-
-
-
- 
-  
-
-
-
-
-
-
-
-
-
 }());
