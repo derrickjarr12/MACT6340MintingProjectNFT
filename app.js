e18ff3f74eb3d33418a20a178589a53b8381e8dc
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-09-21 19:07:55 -0400

    Updated all js files and contact.html for SMtp setup

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
+import * as utils from "./utils/utils.js";
 dotenv.config();
+
+
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// });
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World!')
+// // });
 
-app.post("/mail", (req, res) => {
-  console.log("mail button clicked");
+
+app.post("/mail", async (req, res) => {
+  await utils
+  .sendMessage(req.body.sub, req.body.txt)
+  .then(() => {
+    res.send({result: "success"});
+  })
+  .catch((error) => {
+    res.send({result: "failure"});
+  });
+  // console.log("mail button clicked");
   
 });
 
 app.listen(port, () => {
-  console.log(process.env.SENSITIVE_DATA);
   console.log(`Example app listening on port ${port}`);
 });
--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,28 @@
 import nodemailer from "nodemailer";
 
-function sendMessage(sub, text) {
+export asyncfunction sendMessage(sub, txt) {
+	let transporter = nodemailer.createTransporter({
+		host: process.env.MAIL_HOST,
+		port: process.env.MAIL_PORT,
+		secure: process.env.MAIL_SECURE,
+		auth: {
+			user: process.env.MAIL_USERNAME,
+		},
+		requireTLS: process.env.MAIL_TLS,
+	});
 }
+
+let message ={
+    from: process.env.MESSAGE_FROM,
+    to: process.env.MESSAGE_TO,
+    subject: sub,
+    text: txt,
+};
+await transporter
+.sendMail(message);
+then(() => {
+    console.log("Message sent");
+})
+.catch((error) => {
+    console.log("Message not sent. " + error);
+});
