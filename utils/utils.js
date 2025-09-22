036a46da84f3c0c72bd4cc93a386d85cbc31dfc2
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-09-21 20:58:28 -0400

    nodemon kept crashing

--- utils/utils.js
+++ utils/utils.js
@@ -1,28 +1,31 @@
 import nodemailer from "nodemailer";
 
-export asyncfunction sendMessage(sub, txt) {
-	let transporter = nodemailer.createTransporter({
+export async function sendMessage(sub, txt) {
+	let transporter = nodemailer.createTransport({
 		host: process.env.MAIL_HOST,
 		port: process.env.MAIL_PORT,
 		secure: process.env.MAIL_SECURE,
 		auth: {
 			user: process.env.MAIL_USERNAME,
+            pass: process.env.MAIL_PASSWORD,
 		},
+        
 		requireTLS: process.env.MAIL_TLS,
 	});
 }
 
 let message ={
-    from: process.env.MESSAGE_FROM,
+    from: process.env.MAIL_USERNAME,
     to: process.env.MESSAGE_TO,
     subject: sub,
     text: txt,
+    
 };
 await transporter
-.sendMail(message);
-then(() => {
-    console.log("Message sent");
-})
-.catch((error) => {
-    console.log("Message not sent. " + error);
-});
+    .sendMail(message)
+    .then(() => {
+        console.log("Message sent");
+    })
+    .catch((error) => {
+        console.log("Message not sent. " + error);
+    });
