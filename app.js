264b82a80d84a99160ccd24a349cbb9a638a5380
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-10-01 18:20:46 -0400

    debugging ejs. updated project &projects ejs file

--- app.js
+++ app.js
@@ -11,36 +11,38 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
-
-app. get("/", (_req, res) => {
+app.get("/", (_req, res) => {
   res.render("index.ejs");
 });
+
 app.get("/home", (_req, res) => {
-  res.render("index.ejs")
+  res.render("index.ejs");
 });
 
-app.get("/featured-project", (_req, res) =>
-    res.render("featuredProject.ejs")
-);
+app.get("/featured-project", (_req, res) => {
+  res.render("featuredProject.ejs");
+});
+
 app.get("/gallery", (_req, res) =>
     res.render("gallery.ejs")
 );
+
 app.get("/contact", (_req, res) =>
     res.render("contact.ejs")
 );
 
 app.get('/projects', (_req, res) =>
-    res.render('projects', { projectArray: data })
+    res.render('projects.ejs', { projectArray: data })
 );
 
-app.get("projects/:id", (req, res) => {
+app.get("/project/:id", (req, res) => {
   let id = req.params.id;
   if(id > data.length) {
     throw new Error("No project with that ID");
   }
+  res.render("project.ejs", {projectArray: data, which: id});
 });
+
 app.post("/mail", async (req, res) => {
   await utils
       .sendMessage(req.body.sub, req.body.txt)
@@ -50,9 +52,6 @@ app.post("/mail", async (req, res) => {
       .catch((_error) => {
         res.send({result: "failure"});
       });
-  // console.log("mail button clicked");
-
 });
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
