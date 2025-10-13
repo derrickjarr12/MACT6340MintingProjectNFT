f0ddc6027e093ca5eb4e98bb4e2e374a7a6e84e9
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-10-12 23:45:25 -0400

    started metamask connection in index.js

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,32 @@
 (function() {
     "use strict";
 
+    (( +>)) {
+        let userAddress =null;
+        let connect = document.querySelector('#wallet-connect');
+
+        async function connectWallet() {
+            await (window.ethereum) {
+                .request({ method: 'eth_requestAccounts' })
+                .then(data => {
+                    userAddress = data[0];
+                    let walletString =
+                    userAddress.substring(0, 5) + "..." + userAddress.substring(38, 42);
+                    connect.innerHTML = walletString;
+                    return userAddress;
+                });
+            }.catch ((error) => {
+                if (error.code === 4001) {
+                    console.log('User rejected the request.');
+
+                    console.log("Please connect a wallet.");            
+                } else {
+                    console.error(error);
+                }
+                
+
+    }
+
 document.querySelector("#contact-form-button").addEventListener("click", subitMail);
 
 function subitMail(){
