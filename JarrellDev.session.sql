0de5cad984b93e7478bfe5398ecb3f34b5d2491a
derrickjarr12 <derrickj.jarrell@gmail.com>
2025-10-27 17:40:30 -0400

    testing mysql

--- JarrellDev.session.sql
+++ JarrellDev.session.sql
@@ -1,6 +1,17 @@
-SELECT * FROM projects;
-SELECT project_name, quantity, price_eth FROM projects;
-SELECT * FROM projects WHERE active = 1;
+INSERT INTO projects(project_name, img_url, project_description, quantity, price_eth, open_date_gmt, royalty_percent, active) 
+VALUES 
+('Silver Dreams', 
+ '/images/silver-dreams.jpg', 
+ 'A mesmerizing collection of cosmic digital art featuring nebulas and galaxies.', 
+ 100, 
+ '2.50', 
+ '2025-11-01 12:00:00', 
+ '5.00', 
+ 1);
+
+SELECT * FROM projects WHERE project_name = 'Silver Dreams';
+)
+
 
 
 
